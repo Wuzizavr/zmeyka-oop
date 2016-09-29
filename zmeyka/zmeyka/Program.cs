18028cce1422ccd79c1630fc3389376432914a37
Wuzizavr <wuzizavr@gmail.com>
2016-09-29 19:24:27 +0300

    создали конструкцию

--- zmeyka/zmeyka/Program.cs
+++ zmeyka/zmeyka/Program.cs
@@ -10,17 +10,13 @@ namespace zmeyka
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-            p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';
+            Point p1 = new Point(1, 3, '*');
+        
             p1.Draw();
 
 
-            Point p2 = new Point();
-            p2.x = 3;
-            p2.y = 6;
-            p2.sym = '*';
+            Point p2 = new Point(3, 6, '*');
+           
             p2.Draw();
 
 
